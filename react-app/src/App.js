23842187830dae7352084613290aae17ed2cf1fb
kedar-007 <cakedar7390@gmail.com>
2025-11-06 13:29:39 +0530

    Converted react-app from submodule to normal folder

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Testing the CICD with Zoho catalyst with github
+    </div>
+  );
+}
+
+export default App;
